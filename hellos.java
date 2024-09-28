7ccac11f6bed9c4998d352604e4bc86caaa029a9
Iris Sky <[Kstrother@Guilford.edu]>
2024-09-28 16:17:48 -0400

    hi<3

---
+++ hellos.java
@@ -0,0 +1,47 @@
+import java.time.DayOfWeek;
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        // next line/
+        System.out.println("haiii");
+        // this code helps to print "HELLO WORLD" into the terminal below/
+
+        // STUFF YOU NEED TO KNOWS//
+        // variable//
+        // a placeholder for a value that behaves as the value it contains//
+        // examples//
+        int x = 123; // initilization
+        String Myname = "Iris";
+        byte age = 19;
+        String Hobbies = " playing with code and trying to guitar";
+        String BirthdayMonth = "October";
+        int BirthdayNumber = 03;
+        int BirthdayYear = 2024;
+        String Gender2 = "Girl";
+        String Gender1 = "Boy";
+        String ILOVES = "GIRLSSTUFF";
+        String fakename = "Kyle";
+        String why = "why";
+
+        System.out.println(x);
+        System.out.println("THESE ARE MY INTRODUCTIONS :3");
+        System.out.println("My name is " + Myname);
+        System.out.println( " and ");
+        System.out.println(" am almost " + age + " Years old.");
+        System.out.println(" -MY HOBBIES INCLUDE-");
+        // now lets use .substring(0, 0));// in our code
+        System.out.println(" like" + Hobbies.substring(0, 19));
+        ;
+        System.out.println(Hobbies.substring(19, 39));
+        System.out.println("-MY BIRTHDAY-");
+        System.out.println(
+                "I like to celebrate my birthday on " + BirthdayMonth + " " + BirthdayNumber + " " + BirthdayYear);
+                System.out.println("-ABOUT MYSELFS-");
+        System.out.println(" I always felt like a " + Gender2 + " when I was little");
+        System.out.println("or like");
+        System.out.println(" I had no gendered way of seeing things");
+        System.out.println("its just so happens that i like " + ILOVES);
+    }
+}
+        
